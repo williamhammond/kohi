027c8138bee7bf3b5189f80090edcec2f947acac
William Hammond <william.t.hammond@gmail.com>
2023-10-15 15:50:27 -0400

    Increment current frame on present

--- engine/src/renderer/vulkan/vulkan_swapchain.c
+++ engine/src/renderer/vulkan/vulkan_swapchain.c
@@ -83,6 +83,8 @@ void vulkan_swapchain_present(
         } else if (result != VK_SUCCESS) {
             KFATAL("Failed to acquire swapchain image!");
         }
+
+        context->current_frame = (context->current_frame + 1) % swapchain->max_frames_in_flight;
     }
 
 void create(vulkan_context* context, u32 width, u32 height, vulkan_swapchain* swapchain) {
