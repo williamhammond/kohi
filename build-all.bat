990de6bb1019ba773a7dde539402e30d2732aead
William Hammond <william.t.hammond@gmail.com>
2023-10-16 22:29:54 -0400

    Use Make for incremental builds

--- build-all.bat
+++ build-all.bat
@@ -3,15 +3,24 @@ REM Build Everything
 
 ECHO "Building everything..."
 
+@REM PUSHD engine
+@REM CALL build.bat
+@REM POPD
+@REM IF %ERRORLEVEL% NEQ 0 (echo Error:%ERRORLEVEL% && exit)
 
-PUSHD engine
-CALL build.bat
-POPD
+@REM PUSHD testbed
+@REM CALL build.bat
+@REM POPD
+@REM IF %ERRORLEVEL% NEQ 0 (echo Error:%ERRORLEVEL% && exit)
+
+REM Engine
+make -f "Makefile.engine.windows.mak" all
+
 IF %ERRORLEVEL% NEQ 0 (echo Error: %ERRORLEVEL% && exit)
 
-PUSHD testbed
-CALL build.bat
-POPD
+
+REM Testbed
+make -f "Makefile.testbed.windows.mak" all
 IF %ERRORLEVEL% NEQ 0 (echo Error: %ERRORLEVEL% && exit)
 
 ECHO "All assemblies built successfully."
